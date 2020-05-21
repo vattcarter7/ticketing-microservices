03471c94f3a0f0bab805953e117d6f788ff92dfa
vattcarter7 <vattcarter@gmail.com>
2020-05-22 00:14:30 +0700

    finished authentication in nextjs

--- client/pages/banana.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-  return <h1>Banana Page2 </h1>;
-};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,19 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  axios.get('/api/users/currentuser');
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context) => {
+  console.log('landing page!');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-  return response.data;
+  return data;
 };
 
 export default LandingPage;
