da034f383373648b6c467dfe4a7c991ef234001d
vattcarter7 <vattcarter@gmail.com>
2020-05-23 01:06:06 +0700

    implemented NATS Streaming Service

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,39 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222'
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS!');
+
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
+});
+
+// When client closes the connection
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
