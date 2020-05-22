eccfa5f6a132356a37a8cd32889b5b84a5fae921
vattcarter7 <vattcarter@gmail.com>
2020-05-22 16:51:20 +0700

    added tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@sptickets/common';
+import { createTicketRouter } from './routes/new';
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+
+// all = get | post | put | delete
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
