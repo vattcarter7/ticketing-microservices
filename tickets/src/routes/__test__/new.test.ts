dd588a0a35c7ca118ca809796db8d762efc52b5c
vattcarter7 <vattcarter@gmail.com>
2020-05-24 19:55:01 +0700

    managed a NATS Client and implemted test with mock function

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -79,4 +80,19 @@ it('creates a ticket with valid input', async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(20);
   expect(tickets[0].title).toEqual(title);
+});
+
+it('published an event', async () => {
+  const title = 'slfkjasdflkj';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price: 20
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotAuthorizedError
 } from '@sptickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -35,6 +37,12 @@ router.put(
     });
 
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
 
     res.send(ticket);
   }
