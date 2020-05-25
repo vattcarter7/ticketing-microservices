6c15daa099f1b8093860e6b6c82f594618a67842
vattcarter7 <vattcarter@gmail.com>
2020-05-25 18:21:17 +0700

    implemented tests on order service

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,6 +1,5 @@
 import request from 'supertest';
 import { app } from '../../app';
-import { Order } from '../../models/order';
 import { Ticket } from '../../models/ticket';
 
 const buildTicket = async () => {
