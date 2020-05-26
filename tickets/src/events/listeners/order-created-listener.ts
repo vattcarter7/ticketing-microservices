b6e5b915ea5e96fef3bd5e83fba3378420628bcf
vattcarter7 <vattcarter@gmail.com>
2020-05-26 15:30:04 +0700

    embeding orderId in Ticket Modle

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,30 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@sptickets/common';
+
+import { queueGroupName } from './queueGroupName';
+import { Ticket } from '../../models/ticket';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // if no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // mark the ticket as being reserved by setting its orderI property
+    ticket.set({ orderId: data.id });
+
+    // save the ticket
+    await ticket.save();
+
+    // ack the message
+    msg.ack();
+  }
+}
