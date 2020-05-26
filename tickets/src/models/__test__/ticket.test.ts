96a667c48562bc2f01c9a609882776b9bab41651
vattcarter7 <vattcarter@gmail.com>
2020-05-26 13:12:10 +0700

    implemented optimistic concurrency control

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,49 @@
+import { Ticket } from '../ticket';
+
+it('implements optismistic concurrency control - version control', async (done) => {
+  // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123'
+  });
+
+  // Save the ticket to the database
+  await ticket.save();
+
+  // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make two seperate changes to the tickets we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
+  // Save the first fetched ticket
+  await firstInstance!.save();
+
+  // Save the second fetched ticket and expect an error
+
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return done();
+  }
+
+  throw new Error('Should not reach to this point');
+});
+
+it('increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+    userId: '123'
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
